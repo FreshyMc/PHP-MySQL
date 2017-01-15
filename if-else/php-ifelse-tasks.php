c441059f7f41f9a0663fa9276fa306917d6fa29e
todorovaeva <eva.todorova@gmail.com>
2017-01-15 21:22:10 +0200

    .

---
+++ if-else/php-ifelse-tasks.php
@@ -0,0 +1,27 @@
+<?php
+
+
+// $a = 29;
+// $b = 25;
+
+// if ($a>$b) {
+// 	echo "$b, $a";
+// } else{
+// 	echo "$a, $b";
+// }
+
+//$a = 12;
+$a = rand(1,1000);
+
+if ($a%2==0) {
+	echo "$a number is chetno/odd";
+} else{
+	echo "$a number is nechetno/even";
+}
+
+
+
+
+
+
+
