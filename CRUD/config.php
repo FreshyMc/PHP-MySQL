c7f87bc5dd67a903eed7b9838bb20c8ce7bde7b4
todorovaeva <eva.todorova@gmail.com>
2017-02-27 01:18:00 +0200

    Update config.php

--- CRUD/config.php
+++ CRUD/config.php
@@ -4,14 +4,7 @@
 $conn = mysql_connect("localhost","root","") 
 			or die("cannot connected");
 
-// mysql_select_db("database-name", "connection-link-identifier")
-@mysql_select_db("test",$conn);
-*/
 
-/**
- * mysql_connect is deprecated
- * using mysqli_connect instead
- */
 
 $databaseHost = 'localhost';
 $databaseName = 'test';
